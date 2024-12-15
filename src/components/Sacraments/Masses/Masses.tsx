20c9533308f51c9775b9faf74befdcda843b26fe
Alicja <alicja.zurek@cloudimed.pl>
2024-12-15 14:31:58 +0100

    masses info added

--- src/components/Sacraments/Masses/Masses.tsx
+++ src/components/Sacraments/Masses/Masses.tsx
@@ -1,27 +1,65 @@
 import { FC } from "react";
 import { cardsDescription } from "../../../utils/cardsDescription";
 import styled from "styled-components";
+import { massesInfo } from "../../../utils/massesInfo";
 
 export const Masses: FC = () => {
   return (
     <>
-      <div>
-        <p>{cardsDescription[0].title}</p>
-        {cardsDescription[0].addInfo.map((element) => (
-          <div key={element.title}>
-            <span className="fw-bolder">{element.title}: </span>
-            {element.desc}
-          </div>
-        ))}
-      </div>
-      <div>
-        Codzienna Całodzienna adoracja Najświętszego SAkramentu w kaplicy
-        adoracji
-      </div>
-      <div>
-        <p>Nabożeństwa</p>
-      </div>
+      {/*<div className="mb-2">*/}
+      {/*  <Title className="text-uppercase">{cardsDescription[0].title}</Title>*/}
+      {/*  {cardsDescription[0].addInfo.map((element) => (*/}
+      {/*    <div key={element.title}>*/}
+      {/*      <span className="fw-bolder ">{element.title}: </span>*/}
+      {/*      {element.desc}*/}
+      {/*    </div>*/}
+      {/*  ))}*/}
+      {/*</div>*/}
+      {massesInfo.map((element) => (
+        <div className="mb-2">
+          <Title className="text-uppercase">{element.title}</Title>
+          {element.addInfo.map((element) => (
+            <div key={element.title}>
+              <span className="fw-bolder">{element.title}: </span>
+              {element.desc}
+            </div>
+          ))}
+        </div>
+      ))}
+
+      {/*<Title>*/}
+      {/*  Codzienna Całodzienna adoracja Najświętszego SAkramentu w kaplicy*/}
+      {/*  adoracji*/}
+      {/*</Title>*/}
+      {/*<div>*/}
+      {/*  <Title className="text-uppercase">Nabożeństwa</Title>*/}
+      {/*  <div>*/}
+      {/*    Pierwszy czwartek miesiąca: nabożeństwo po mszy świętej wieczorowej*/}
+      {/*    Pierwszy piątek: nabożeństwo po mszy świętej wieczorowej KOMUNIA ŚW.*/}
+      {/*    DLA CHORYCH W każdy pierwszy piątek miesiąca od godziny 9.00 księża z*/}
+      {/*    naszej parafii odwiedzają chorych w ich domach, służąc im posługą*/}
+      {/*    sakramentalną: zanoszą chorym Komunię św., jest też możliwość*/}
+      {/*    spowiedzi, a także przyjęcia sakramentu namaszczenia. Pierwszy sobota*/}
+      {/*    miesiąca: nabożeńśtwo po mszy świętej o godzinie 7:30 W każdy wtorek*/}
+      {/*    modlitwy do Świetego Antoniego Padewskiego W każdą srodę nowenna do*/}
+      {/*    Matki Bożej Nieustającej Pomocy o godzinie 17 W miesiącu maj*/}
+      {/*    codziennie nabożeństwa do Matki Bożej po mszy świętej wieczorowej W*/}
+      {/*    miesiącu czerwiec codziennie nabożeństwa do Serca Pana Jezusa po mszy*/}
+      {/*    świętej wieczorowej W miesiącu paźniernik codziennie nabożeństwa*/}
+      {/*    różańcowe po mszy świętej wieczorowej adwent - roraty w dni powszednie*/}
+      {/*    na mszy swietej wieczorowej W okresie Wielkiego Postu droga krzyżowa w*/}
+      {/*    piątki: dla dzieci przed mszą świętą wieczorową o godzinie 16:30, dla*/}
+      {/*    dorosłych po mszy wieczorowej, gorzkie żale w niedziele o godzinie*/}
+      {/*    16:30 Nabożeństwa fatimskie od maja do października każego 13 dnia*/}
+      {/*    miesiąca Doba Eucharystyczna: 15/16 sierpnia Odpust: Najświętszej*/}
+      {/*    Maryii Panny Królowej Polski - 3. maja św. Michała Archanioła 29.*/}
+      {/*    września*/}
+      {/*  </div>*/}
+      {/*</div>*/}
     </>
   );
 };
-const Title = styled.span``;
+const Title = styled.p`
+  font-weight: bolder;
+  font-size: 1.2rem;
+`;
---
+++ src/utils/massesInfo.ts
@@ -0,0 +1,103 @@
+export const massesInfo = [
+  {
+    title: "Msze Święte",
+    addInfo: [
+      {
+        title: "w niedziele i uroczystości",
+        desc: "7:30, 9:30, 10:45 (z udziałem dzieci), 12:00,17:00",
+      },
+      { title: "święta", desc: "8:00, 10:00, 17:00" },
+      { title: "dni powszednie", desc: "7:00, 7:30, 17:00" },
+    ],
+  },
+  {
+    title:
+      "Codzienna Całodzienna adoracja Najświętszego SAkramentu w kaplicy adoracji",
+    addInfo: [],
+  },
+  {
+    title: "Nabożeństwa",
+    addInfo: [
+      {
+        title: "pierwszy czwartek miesiąca",
+        desc: "nabożeństwo po mszy świętej wieczorowej",
+      },
+      {
+        title: "pierwszy piątek miesiąca",
+        desc: "nabożeństwo po mszy świętej wieczorowej",
+      },
+      {
+        title: "KOMUNIA ŚW. DLA CHORYCH",
+        desc: "W każdy pierwszy piątek miesiąca od godziny 9.00 księża z naszej parafii odwiedzają chorych w ich domach, służąc im posługą sakramentalną: zanoszą chorym Komunię św., jest też możliwość spowiedzi, a także przyjęcia sakramentu namaszczenia",
+      },
+      {
+        title: "pierwsza sobota miesiąca",
+        desc: "nabożeństwo po mszy świętej o godzinie 7:30",
+      },
+      {
+        title: "w każdy wtorek",
+        desc: "modlitwy do Świetego Antoniego Padewskiego",
+      },
+      {
+        title: "w każdą środę",
+        desc: "nowenna do Matki Bożej Nieustającej Pomocy o godzinie 17",
+      },
+    ],
+  },
+  {
+    title: "",
+    addInfo: [
+      {
+        title: "w miesiącu maj",
+        desc: "codziennie nabożeństwa do Matki Bożej po mszy świętej wieczorowej",
+      },
+      {
+        title: "w miesiącu czerwiec",
+        desc: "codziennie nabożeństwa do Serca Pana Jezusa po mszy świętej wieczorowej",
+      },
+      {
+        title: "w miesiącu październik",
+        desc: "codzienne nabożeństwa różańcowe po mszy świętej wieczorowej",
+      },
+      {
+        title: "adwent",
+        desc: "roraty w dni powszednie na mszy świętej wieczorowej",
+      },
+      {
+        title: "w okresie Wielkiego Postu",
+        desc: "droga krzyżowa w piątki: dla dzieci przed mszą świętą wieczorową o godzinie 16:30, dla dorosłych po mszy wieczorowej, gorzkie żale w niedziele o godzinie 16:30",
+      },
+    ],
+  },
+  {
+    title: "",
+    addInfo: [
+      {
+        title: "Nabożeństwa fatimskie",
+        desc: "od maja do października każdego 13 dnia miesiąca",
+      },
+    ],
+  },
+  {
+    title: "",
+    addInfo: [
+      {
+        title: "doba eucharystyczna",
+        desc: "15/16 sierpnia",
+      },
+    ],
+  },
+  {
+    title: "Odpust",
+    addInfo: [
+      {
+        title: "Najświętszej Maryii Panny Królowej Polski",
+        desc: "3 maja",
+      },
+      {
+        title: "św. Michała Archanioła",
+        desc: "29. września",
+      },
+    ],
+  },
+];
