039c11f1c3ac854e908e62d3dd6391574cb596f9
Alicja <alicja.zurek@cloudimed.pl>
2025-02-09 13:46:59 +0100

    intentions and ands added

--- src/utils/adsInfo.ts
+++ src/utils/adsInfo.ts
@@ -1,12 +1,10 @@
 export const adsInfo = {
-  date: "02 II 2025",
+  date: "09 II 2025",
   info: [
-    "Przeżywamy święto Ofiarowania Pańskiego.",
-    "Dziś o godz.16.00 będzie miesięczna adoracja Najświętszego Sakramentu i zmiana tajemnic Żywego Różańca.",
-    "W poniedziałek na mszach św. będzie błogosławieństwo św. Błażeja, patrona chorób gardła.",
-    "W środę o godz. 17.00 będzie msza św. zbiorowa ku czci Matki Bożej Nieustającej Pomocy.",
-    "W tym tygodniu przypadają I czwartek i I piątek miesiąca z odpowiednimi nabożeństwami. W piątek o godz. 9.00 odwiedzimy z komunią św. chorych. Prosimy cierpliwie czekać bo będzie posługiwał jeden kapłan. Spowiedź pierwszopiątkowa w kościele o godz. 16.00.",
-    "11 lutego przypada XXXIII Światowy Dzień Chorego, w liturgiczne wspomnienie Matki Bożej z Lourdes. Z tej okazji w przyszłą niedzielę będą zbierane ofiary do puszki na chorych, którymi opiekuje się ruch dobroczynny „Betel” z Częstochowy.",
-    "Dziękujemy za ofiary na renowację ławek. Ofiary złożyły pewne osoby. Bóg zapłać.",
+    " Przeżywamy V niedzielę zwykłą.",
+    'Z okazji przypadającego we wtorek Światowego Dnia Chorego dziś zbierane są ofiary na chorych i niepełnosprawnych, którymi opiekuje się ruch dobroczynny „Betel". Ofiary można złożyć do puszki przy wyjściu z kościoła.',
+    "We wtorek o godz. 17.00 będzie msza św. zbiorowa za wstawiennictwem św. Antoniego.",
+    "W przyszłą niedzielę taca przeznaczona będzie na nasze inwestycje parafialne.",
+    "W pryszłą niechielę o 10:45 będzie Mesza św. Urodzinowa",
   ],
 };
--- src/utils/massesIntentions.ts
+++ src/utils/massesIntentions.ts
@@ -1,147 +1,6 @@
 export const massesIntentions = [
   {
     day: "Niedziela",
-    date: "02.02.25r.",
-    addInfo: "",
-    intensions: [
-      {
-        hour: "7:30",
-        intention: "+Mirosława Rakusa /od uczest. pogrzebu/",
-      },
-      {
-        hour: "9:30",
-        intention: "+Juliana Kożucha",
-      },
-      {
-        hour: "10:45",
-        intention: "+Feliksa Woźniaka",
-      },
-      {
-        hour: "12:00",
-        intention: "Za parafian",
-      },
-      {
-        hour: "17:00",
-        intention: "+Zanonę, Mariana Ujmów, Ewę Glińską",
-      },
-    ],
-  },
-  {
-    day: "Poniedziałek",
-    date: "03.02.25r.",
-    addInfo: "",
-    intensions: [
-      {
-        hour: "7:00",
-        intention: "+Roberta Nowiszewskiego /od uczest. pogrzebu/",
-      },
-      {
-        hour: "7:30",
-        intention: "+Mariana Kicińskiego /od rodz. Kitlińskich/",
-      },
-      {
-        hour: "17:00",
-        intention:
-          "+Zofię, Wiktorię, Jana Podolaków, Bronisławę, Stanisława Okłów",
-      },
-    ],
-  },
-  {
-    day: "Wtorek",
-    date: "04.02.25.",
-    addInfo: "",
-    intensions: [
-      {
-        hour: "7:00",
-        intention: "+Irenę Dziwińską /od sąsiadki Haliny Pileckiej/",
-      },
-      {
-        hour: "7:30",
-        intention: "+Roberta Nowiszewskiego /od uczest. pogrzebu/",
-      },
-      {
-        hour: "17:00",
-        intention: "+Mirosława Rakusa /od żony Małgorzaty i córki Wiktorii/",
-      },
-    ],
-  },
-  {
-    day: "Środa",
-    date: "05.02.25r.",
-    intensions: [
-      {
-        hour: "7:00",
-        intention: "O Boże błogosławieństwo i zdrowie dla Agaty",
-      },
-      {
-        hour: "7:30",
-        intention: "+Elżbietę Borowską /od męża Krzysztofa/",
-      },
-      {
-        hour: "17:00",
-        intention: "Zbiorowa",
-      },
-    ],
-  },
-  {
-    day: "Czwartek",
-    date: "06.02.25.",
-    addInfo: "",
-    intensions: [
-      {
-        hour: "7:00",
-        intention: "+Irenę Dziwińską /od siostry Krystyny ze Szczecina/",
-      },
-      {
-        hour: "7:30",
-        intention: "Za kapłanów",
-      },
-      {
-        hour: "17:00",
-        intention: "+Sylwestra Lamcha /od syna Huberta z Martyną/",
-      },
-    ],
-  },
-  {
-    day: "Piątek",
-    date: "07.02.25.",
-    addInfo: "",
-    intensions: [
-      {
-        hour: "7:00",
-        intention: "+Elżbietę Borowską /od rodz. Gworysów/",
-      },
-      {
-        hour: "7:30",
-        intention: "+Sylwestra Lamcha /od wnuczki Kingi/",
-      },
-      {
-        hour: "17:00",
-        intention: "+Kazimierę Kulejewską /od Stanisławy Kulejewskiej z rodz./",
-      },
-    ],
-  },
-  {
-    day: "Sobota",
-    date: "08.02.25r.",
-    addInfo: "",
-    intensions: [
-      {
-        hour: "7:00",
-        intention: "+Elżbietę Borowską /od rodz. Kościańskich i Lengnerów/",
-      },
-      {
-        hour: "7:30",
-        intention: "+Mariana Kicińskiego /od szwagierki Grażyny z rodz./",
-      },
-      {
-        hour: "17:00",
-        intention: "+Anielę /1 r. śm/, Henryka /46 r. śm./ Walaszczyków",
-      },
-    ],
-  },
-  {
-    day: "Niedziela",
     date: "09.02.25r.",
     addInfo: "",
     intensions: [
@@ -164,6 +23,150 @@ export const massesIntentions = [
       {
         hour: "17:00",
         intention: "+Franciszka, Ryszarda Leśko",
+      },
+    ],
+  },
+  {
+    day: "Poniedziałek",
+    date: "10.02.25r.",
+    addInfo: "",
+    intensions: [
+      {
+        hour: "7:00",
+        intention: "+Elżbietę Borowską /od rodz. Małasiewiczów/",
+      },
+      {
+        hour: "7:30",
+        intention: "+Ryszarda Chłąda /od Janiny i Piotra Jagielskich/",
+      },
+      {
+        hour: "17:00",
+        intention:
+          "+Roberta Nowiszewskiego /od kolegi Andrzeja Jamrozińskiego z rodz./",
+      },
+    ],
+  },
+  {
+    day: "Wtorek",
+    date: "11.02.25.",
+    addInfo: "",
+    intensions: [
+      {
+        hour: "7:00",
+        intention: "+Danielę Gosławską /od siostrzenicy Eli/",
+      },
+      {
+        hour: "7:30",
+        intention: "+Elżbietę Borowską /od chrześnicy Magdaleny z rodz./",
+      },
+      {
+        hour: "17:00",
+        intention: "Zbiorowa",
+      },
+    ],
+  },
+  {
+    day: "Środa",
+    date: "12.02.25r.",
+    intensions: [
+      {
+        hour: "7:00",
+        intention: "+Mirosława Korbla /od żony Jolanty z córką Justyną/",
+      },
+      {
+        hour: "7:30",
+        intention:
+          "+Elżbietę Borowską /od Kazimierza Słabika i Zofii Bruzda z rodz./",
+      },
+      {
+        hour: "17:00",
+        intention: "+Antoniego, Andrzeja Cieślików",
+      },
+    ],
+  },
+  {
+    day: "Czwartek",
+    date: "13.02.25.",
+    addInfo: "",
+    intensions: [
+      {
+        hour: "7:00",
+        intention: "+Jana Korbuta /od brata Jerzego z żoną Ewą/",
+      },
+      {
+        hour: "7:30",
+        intention: "+Elżbietę Borowską /od rodz. Parkitnych/",
+      },
+      {
+        hour: "17:00",
+        intention:
+          "+Roberta Nowiszewskiego /od Jadwigi, Andrzeja, Janka Nowaków/",
+      },
+    ],
+  },
+  {
+    day: "Piątek",
+    date: "14.02.25.",
+    addInfo: "",
+    intensions: [
+      {
+        hour: "7:00",
+        intention:
+          "O Boże bł., zdrowie i wszelkie łaski dla Magdaleny z okazji urodzin",
+      },
+      {
+        hour: "7:30",
+        intention: "+Jana Korbuta /od siostry Krystyny z rodz./",
+      },
+      {
+        hour: "17:00",
+        intention: "rez. Grabara",
+      },
+    ],
+  },
+  {
+    day: "Sobota",
+    date: "15.02.25r.",
+    addInfo: "",
+    intensions: [
+      {
+        hour: "7:00",
+        intention: "+Elżbietę Borowską /od Wojciecha Toll z rodz./",
+      },
+      {
+        hour: "7:30",
+        intention: "+Mirosława Korbla /od syna Dariusza z Adrianną/",
+      },
+      {
+        hour: "17:00",
+        intention: "+Józefa, Krystynę, Mikołaja Wineckich",
+      },
+    ],
+  },
+  {
+    day: "Niedziela",
+    date: "16.02.25r.",
+    addInfo: "",
+    intensions: [
+      {
+        hour: "7:30",
+        intention: "+Zofię, Piotra, Antoniego Kalemba",
+      },
+      {
+        hour: "9:30",
+        intention: "+Walętynę Pawłow",
+      },
+      {
+        hour: "10:45",
+        intention: "Urodzinowa: /Kornelia Koralik 10 r. urodzin/",
+      },
+      {
+        hour: "12:00",
+        intention: "Za parafian",
+      },
+      {
+        hour: "17:00",
+        intention: "+Józefa Bisia /2 r. śm., od żony z rodz./",
       },
     ],
   },
